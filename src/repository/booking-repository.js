e3cd3feb479fed2ecb42332b13d49d07349cd120
vraj-hash <202201328@daiict.ac.in>
2025-09-21 02:45:57 +0530

    Added create booking repository code

---
+++ src/repository/booking-repository.js
@@ -0,0 +1,28 @@
+const { Booking } = require("../models/index");
+
+const { AppError, ValidationError } = require("../utils/errors/index");
+
+const { StatusCodes } = require("http-status-codes");
+
+class BookingRepository {
+  async create(data) {
+    try {
+      const booking = await Booking.create(data);
+      return booking;
+    } catch (error) {
+      if (error.name == "SequelizeValidationError") {
+        throw new ValidationError(error);
+      }
+      throw new AppError(
+        "Repository Error",
+        "Cannot Create Booking",
+        "There was some issue creating the booking, please try again",
+        StatusCodes.INTERNAL_SERVER_ERROR
+      );
+    }
+  }
+
+  
+}
+
+module.exports = BookingRepository;
